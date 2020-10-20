2d27bbf66da660c2d2fbcda7a8520f3ff5c5e91c
Antonio J.Sánchez <ajsb.ies@gmail.com>
2020-10-20 19:49:51 +0200

    Initial commit

---
+++ app/src/main/java/com/ajsb/holamundo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ajsb.holamundo;
+
+/**
+ * Programación Multimedia y de Dispositivos Móviles
+ * @author Antonio J.Sánchez
+ */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
